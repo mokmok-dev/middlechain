c4e72a05ae1a0af1e1bd324a8849d79cb4592235
Pranc1ngPegasus <ride.or.die.2215@icloud.com>
2022-11-06 07:00:58 +0900

    feat: implement http middleware chainer

---
+++ middlechain.go
@@ -0,0 +1,13 @@
+package middlechain
+
+import (
+	"net/http"
+)
+
+func Chain(h http.Handler, m ...func(http.Handler) http.Handler) http.Handler {
+	if len(m) == 0 {
+		return h
+	}
+
+	return m[0](Chain(h, m[1:cap(m)]...))
+}
